7649492d0c4afe1f36f17d5414acb76cb4c351af
JoseFlores <jlfl94@gmail.com>
2023-06-05 14:45:52 -0400

    Add template view when apply template images

--- resources/views/ticketDownload.blade.php
+++ resources/views/ticketDownload.blade.php
@@ -13,16 +13,37 @@
         .line-bottom{
             border-bottom: 2px dotted #000000;
         }
+        body{
+            padding-top:25px
+        }
 
     </style>
 </head>
 
 <body>
+                        
+                    {{-- <div style="position:relative;">
+                            @foreach ($data as $item)
+                                    <div style="position: relative;margin-bottom:50px">
+                                        <!-- Imagen del usuario -->
+                                        <img src="{{$image}}" alt="Imagen del usuario">
+
+                                        <!-- Código QR -->
+                                        <div style="position: absolute; top: 5px; left: 500px;"> {!! DNS2D::getBarcodeHTML("$item->code", 'QRCODE',7,7) !!}</div>
+                                    </div>
+                                </div>
+                            @endforeach
+                    </div> --}}
+                                
+
+
+
                 <table class="table">
                     @foreach ($data as $item)
                         
                         @if($item->type == 'QR')
                     <tbody>
+                    
                             <tr class="line-top">
                                 <td style="color:transparent">-</td>
                                 <td style="color:transparent">-</td>
@@ -30,7 +51,6 @@
                             </tr>
                             <tr>
                                 <td>
-                                    {{-- <img src="{{storage_path().'/app/public/'.$image}}" style="width: 100px; height: 100px"> --}}
                                     <img src="{{$image}}" style="width: 100px; height: auto">
                                 
                                 </td>
