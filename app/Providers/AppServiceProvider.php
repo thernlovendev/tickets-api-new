93d8904b3d31b6fffc5331988029ef7b036ca3c3
hiep <vanhiep2102@gmail.com>
2023-07-04 22:41:44 +0700

    fix request validate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('integer_or_nullable', function ($attribute, $value, $parameters, $validator) {
+            if ($value === null) {
+                return true;
+            }
+            return is_numeric($value) && floor($value) == $value;
+        });
     }
 }
