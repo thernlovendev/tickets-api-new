bf37943acf81e80b5ecec6f6f7d99a2a9df78b13
JoseFlores <jlfl94@gmail.com>
2023-07-14 15:03:49 -0400

    add edit Company request validation

--- app/Http/Requests/UserByAdminRequest.php
+++ app/Http/Requests/UserByAdminRequest.php
@@ -60,7 +60,7 @@ class UserByAdminRequest extends FormRequest
                     'password_confirmation' => ['nullable','max:55'],
                     'phone' => ['required','max:15'],
                     'role' => ['required','exists:roles,id'],
-                    'company_id' => ['required']
+                    'company_id' => ['required','exists:companies,id']
                 ];
             } break;
 
