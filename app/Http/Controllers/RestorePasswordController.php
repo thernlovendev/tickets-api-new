d089d1068c606f4ed330ccce4969a4c7ac7ab73c
JoseFlores <jlfl94@gmail.com>
2023-10-30 15:50:13 -0400

    change symbol of forguet password param

--- app/Http/Controllers/RestorePasswordController.php
+++ app/Http/Controllers/RestorePasswordController.php
@@ -8,6 +8,6 @@ class RestorePasswordController extends Controller
 {
     public function showResetPasswordForm($token,$email) { 
         $url = env('APP_URL_WEB_PAGE');
-        return redirect($url.'/#/user/create-password?token='.$token.'?email='.$email)->with(['token' => $token]);
+        return redirect($url.'/#/user/create-password?token='.$token.'&email='.$email)->with(['token' => $token]);
     }
 }
