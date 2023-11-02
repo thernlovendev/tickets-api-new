1fc0eeda61ea03aee176efd436cf12ecce3f3750
JoseFlores <jlfl94@gmail.com>
2023-11-02 10:32:36 -0400

    Fix template of email on reset password

--- resources/views/email/deleteMyUser.blade.php
+++ resources/views/email/deleteMyUser.blade.php
@@ -6,29 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- <link href="https://file.myfontastic.com/6tkDvaBT8S52S4nU8THupM/icons.css" rel="stylesheet"> -->
     <!-- <link rel="stylesheet" href="css/styles.css"> -->
-    <title>{{__('Delete my account')}}</title>
+    <title>{{__('Delete User Mail')}}</title>
     <style>
     body { font-family: 'Poppins', sans-serif; }
     </style>
 </head>
 
-@if($template->content == 'default')
 <body style="margin: 0;
     box-sizing: border-box;
-    background-color: rgb(224, 224, 224);
+    background-color: #fff;
     justify-content: center;
     align-items: center;">
     <div style="margin: 0 auto;
         box-sizing: border-box;
-        width: 60%;
+        width: 70%;
         height: 80%;
         background-color: #fff;
         border-radius: 5px;
         justify-content: center;
-        border-bottom: 0.5em solid #71C8FC;
         padding-bottom: 1.2em;
         margin-bottom: 0.5em;
         min-width: 320px;">
+        
         <div style="margin: 0 auto;
         box-sizing: border-box;
         width: 100%;
@@ -40,6 +39,85 @@
                 width: 100%;
                 max-width: 300px;" src="https://tamice.com/site/images/tamice/logo/tamice-logo-225px.png" alt="logo-joga">
         </div>
+        <table style="margin-left:-45px">
+            <tr>
+                <td style="padding: 4px;">
+                    <div style=" width: 100px; height: 100px; display: inline-block;">
+                    <a style="margin: 0;
+                    box-sizing: border-box;
+                    color:#A3A3A3;
+                    text-decoration: none;
+                    font-weight: bold;
+                    font-size: 0.6em;
+                    margin-right: 6.6em" href="https://tamice.com/searchproduct">
+                                <img style="margin-left: 20px;" src="https://testing.thernloven.com/tickets-api-new/public/images/dashboard-square.svg" alt="icon booking" >
+                    </a>
+                    <br>
+                       <span>My bookings</span>
+                    </div>
+                </td>
+                <td style="padding: 4px;">
+                    <div style=" width: 115px; height: 100px; display: inline-block;">
+                        <a style="margin: 0;
+                        box-sizing: border-box;
+                        color:#A3A3A3;
+                        text-decoration: none;
+                        font-weight: bold;
+                        font-size: 0.6em;
+                        margin-right: 6.6em" href="https://accounts.kakao.com/login/?continue=http%3A%2F%2Fpf.kakao.com%2F_AAelu%2Fchat#login">
+                                    <img style="margin-left: 25px;" src="https://testing.thernloven.com/tickets-api-new/public/images/message.svg" alt="icon Message" >
+                        </a>
+                        <br>
+                            <span style="font-size: 0.9em;" >카톡 상담원 채팅</span>
+                    </div>
+                </td>
+                <td style="padding: 4px;">
+                    <div style=" width: 125px; height: 100px; display: inline-block;">
+                        <a style="margin: 0;
+                        box-sizing: border-box;
+                        color:#A3A3A3;
+                        text-decoration: none;
+                        font-weight: bold;
+                        font-size: 0.6em;
+                        margin-right: 6.6em" href="#">
+                                    <img style="margin-left: 30px;" src="https://testing.thernloven.com/tickets-api-new/public/images/book-open.svg" alt="icon Book Open" >
+                        </a>
+                        <br>
+                            <span style="font-size: 0.9em;" >빅애플패스 이용방법 </span>
+                    </div>
+                </td>
+                <td style="padding: 4px;">
+                    <div style=" width: 125px; height: 100px; display: inline-block;">
+                    <a style="margin: 0;
+                                box-sizing: border-box;
+                                color:#A3A3A3;
+                                text-decoration: none;
+                                font-weight: bold;
+                                font-size: 0.6em;
+                                margin-right: 6.6em" href="#">
+                                    <img style="margin-left: 30px;" src="https://testing.thernloven.com/tickets-api-new/public/images/dollar-circle.svg" alt="icon Money" >
+                        </a>
+                        <br>
+                            <span style="font-size: 0.9em;" >취소 및 환불규정 </span>
+                    </div>
+                </td>
+                <td style="padding: 4px;">
+                    <div style=" width: 150px; height: 100px; display: inline-block;">
+                     <a style="margin: 0;
+                                box-sizing: border-box;
+                                color:#A3A3A3;
+                                text-decoration: none;
+                                font-weight: bold;
+                                font-size: 0.6em;" href="#">
+                                    <img style="margin-left: 30px;" src="https://testing.thernloven.com/tickets-api-new/public/images/location.svg" alt="icon Location" >
+                        </a>
+                        <br>
+                        <span style="font-size: 0.9em;" >타미스 오시는길 </span>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
         <div style="margin: 0;
                 box-sizing: border-box;
                 padding: 0 3em 0 3em">
@@ -49,72 +127,79 @@
                 font-family: sans-serif;
                 font-size: 0.9em;
                 line-height: 1.5"> {{__('Hi!')}} {{$fullname}}, <br>
-                {{__('Your Account has been deleted successfully')}}:</p>
-               <p style="margin: 0;
-                        box-sizing: border-box;
-                        color: #5D5D5F;
-                        font-family: sans-serif;
-                        font-size: 0.9em;
-                        line-height: 1.5;
-                        padding-bottom: 1em">{{__('Thanks')}},<br>
-                {{env('APP_NAME')}} Team</p>
+                @if($template->content == 'default')
+                {{__('Your account was Deleted successfully')}}:
+            </p>
+                @else
+                <p style="margin: 0;
+                box-sizing: border-box;
+                color: #5D5D5F;
+                font-family: sans-serif;
+                font-size: 0.9em;
+                line-height: 1.5">
+                {!!$template->content!!}
+                </p>
+            </p>
+                @endif
                 <footer style="margin: 0;
                         box-sizing: border-box;
                         width: 100%;
                         justify-content: center;
                         border-top: 1px solid #A3A3A3;
-                        padding-top: 1em;
-                        justify-content: space-between;">
-                    <div style="margin: 0;
-                        box-sizing: border-box;
-                        display: inline-block;">
+                        padding-top: 1em;">
+                    <div style="justify-content: space-between;">
+                        <div style="margin: 0;
+                            box-sizing: border-box;
+                            display: inline-block;">
+                        </div>
+                        <div style="margin: 0 auto;
+                            box-sizing: border-box;
+                            display: inline-block;
+                            float: right">
+                            <a style="margin: 0;
+                            box-sizing: border-box;
+                            color:#A3A3A3;
+                            text-decoration: none;
+                            font-weight: bold;
+                            font-size: 0.8em;
+                            margin-right: 0.5em" href="https://blog.naver.com/tamice">
+                                <img style="margin: 0 auto; width: 20px; height: 20px" src="https://testing.thernloven.com/tickets-api-new/public/images/circle-blog.png">
+                            </a>
+                            <a style="margin: 0;
+                            box-sizing: border-box;
+                            color:#A3A3A3;
+                            text-decoration: none;
+                            font-weight: bold;
+                            font-size: 0.8em;
+                            margin-right: 0.5em" href="https://www.facebook.com/NYTamice">
+                                <img style="margin: 0 auto; width: 20px; height: 20px" src="https://testing.thernloven.com/tickets-api-new/public/images/circle-facebook.png">
+                            <a style="margin: 0;
+                            box-sizing: border-box;
+                            color:#A3A3A3;
+                            text-decoration: none;
+                            font-weight: bold;
+                            font-size: 0.8em;
+                            margin-right: 0.5em" href="https://www.instagram.com/with.tamice">
+                                <img style="margin: 0 auto; width: 20px; height: 20px" src="https://testing.thernloven.com/tickets-api-new/public/images/circle-instagram.png">
+                            </a>
+                            <a style="margin: 0;
+                            box-sizing: border-box;
+                            color:#A3A3A3;
+                            text-decoration: none;
+                            font-weight: bold;
+                            font-size: 0.8em;" href="#"></a>
+                        </div>           
                     </div>
-                    <div style="margin: 0 auto;
-                        box-sizing: border-box;
-                        display: inline-block;
-                        float: right">
-                        <a style="margin: 0;
-                        box-sizing: border-box;
-                        color:#A3A3A3;
-                        text-decoration: none;
-                        font-weight: bold;
-                        font-size: 0.8em;
-                        margin-right: 0.5em" href="https://blog.naver.com/tamice">
-                            <img style="margin: 0 auto; width: 13px; height: 20px" src="https://tamice.com/site/images/v2/ico/sns_circle_blog.png">
-                        </a>
-                        <a style="margin: 0;
-                        box-sizing: border-box;
-                        color:#A3A3A3;
-                        text-decoration: none;
-                        font-weight: bold;
-                        font-size: 0.8em;
-                        margin-right: 0.5em" href="https://www.facebook.com/NYTamice">
-                            <img style="margin: 0 auto; width: 13px; height: 20px" src="https://i.imgur.com/9fberpr.png">
-                        <a style="margin: 0;
-                        box-sizing: border-box;
-                        color:#A3A3A3;
-                        text-decoration: none;
-                        font-weight: bold;
-                        font-size: 0.8em;
-                        margin-right: 0.5em" href="https://www.instagram.com/with.tamice">
-                            <img style="margin: 0 auto; width: 16px; height: 21px" src="https://joga.nyc3.digitaloceanspaces.com/icons/email_instagram.png">
-                        </a>
-                        <a style="margin: 0;
-                        box-sizing: border-box;
-                        color:#A3A3A3;
-                        text-decoration: none;
-                        font-weight: bold;
-                        font-size: 0.8em;" href="#"></a>
+                    <div style="justify-content: center; text-align: center">
+                        <span>@2023 Tamice INC Privacy Policy</span>
+                    </div>
+                    <div style="justify-content: center; text-align: center;margin-left: -84px;">
+                        <span>151 West 46th Street, Suite 1002, New York, NY 10036</span>
                     </div>
                 </footer>
+                <div></div>
         </div>
     </div>
     
 </body>
-@else
-
-<body>
-    {!!$template->content!!}
-</body>
-@endif
 </html>
