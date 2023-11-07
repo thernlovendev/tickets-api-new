ebb7f1c4263f60c38e06c3ee0eeb5fec9aa1de89
JoseFlores <jlfl94@gmail.com>
2023-11-07 14:02:55 -0400

    add name to product seats

---
+++ database/migrations/2023_11_07_104015_add_name_to_product_seats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNameToProductSeatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('product_seats', function (Blueprint $table) {
+            $table->string('name')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product_seats', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
+    }
+}
