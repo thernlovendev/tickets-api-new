c2a43a9ac30e70c70a36390c896283af6df205f6
jonathanwambua <jonathanwambua10219@gmail.com>
2023-11-30 05:13:08 +0300

    Update: json conversion

--- app/Services/BroadwayMusicals/src/StructType/MainResponseFormat.php
+++ app/Services/BroadwayMusicals/src/StructType/MainResponseFormat.php
@@ -6,46 +6,27 @@ class MainResponseFormat
 {
     public function convertXmlToJson($xml, $type)
     {
-        $response = [];
-        if ($type === "CityList")
-        {
-            $response = $this->cityList($xml);
-        }
-        if ($type === "Select")
-        {
-            $response = $this->Select($xml);
-        }
-        if ($type === "Buy")
-        {
-            $response = $this->Buy($xml);
-        }
-        return $response;
-    }
-
-    private function cityList($xml)
-    {
-        $data = simplexml_load_string($xml->CityListResult->any);
+        $result_str = $type."Result";
+        $data = simplexml_load_string($xml->$result_str->any);
         $data = json_encode($data);
         $data = json_decode($data);
-        $data = $data->NewDataSet->Table;
-        return $data;
-    }
 
-    private function Select($xml)
-    {
-        $data = simplexml_load_string($xml->SelectResult->any);
-        $data = json_encode($data);
-        $data = json_decode($data);
-        $data = $data->NewDataSet;
-        return $data;
-    }
+        if (!isset($data->NewDataSet->Table))
+        {
+            return $data->NewDataSet->Table1;
+        }
 
-    private function Buy($xml)
-    {
-        $data = simplexml_load_string($xml->BuyResult->any);
-        $data = json_encode($data);
-        $data = json_decode($data);
-        $data = $data->NewDataSet;
-        return $data;
+        $resultArray = array();
+        
+        foreach ($data->NewDataSet->Table as $table)
+        {
+            $temp = [];
+            foreach ($table as $key => $value) {
+                $temp[$key] = (string) $value;
+            }
+            $resultArray[] = $temp;
+        }
+
+        return $resultArray;
     }
 }
